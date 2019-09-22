53b49ae6513962c70a1e194312697e2074ea0715
SAMMY.O <rimit_rifrain@yahoo.co.jp>
2019-09-22 15:48:45 +0900

    create vue frame

---
+++ resources/js/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <main>
+          <div class="container">
+              <RouterView />
+          </div>
+      </main>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+
+</style>
